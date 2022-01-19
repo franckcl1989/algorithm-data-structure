f1360fde7e35c3f3359549994515420579a18f65
Franck <franckcl@icloud.com>
2022-01-19 15:50:16 +0800

    Add linked

--- src/lib.rs
+++ src/lib.rs
@@ -27,18 +27,73 @@ mod tests {
     #[test]
     // 单链表 反转 有序合并 删除倒数第 N 节点 获取中间节点
     fn linked() {
+        // 创建压入
         let mut v: Linked<i32> = linked::Linked::new();
         v.push(0);
         v.push(1);
         v.push(2);
         v.push(3);
-        // println!("{:?}", v);
-        // v.pop();
-        // println!("{:?}", v);
-        // v.pop();
-        // println!("{:?}", v);
-        // v.pop();
-        // println!("{:?}", v);
-        println!("{:?}", v.index(1));
+        println!("{:?}", v);
+        // 索引查找
+        assert_eq!(Some(0), v.index(0));
+        assert_eq!(Some(1), v.index(1));
+        assert_eq!(Some(2), v.index(2));
+        assert_eq!(Some(3), v.index(3));
+        assert_eq!(None, v.index(4));
+        // 删除最后
+        assert_eq!(Some(3), v.pop());
+        println!("{:?}", v);
+        assert_eq!(Some(2), v.pop());
+        println!("{:?}", v);
+        assert_eq!(Some(1), v.pop());
+        println!("{:?}", v);
+        assert_eq!(Some(0), v.pop());
+        println!("{:?}", v);
+        v.push(0);
+        v.push(1);
+        v.push(2);
+        v.push(3);
+        println!("{:?}", v);
+        // 索引删除
+        assert_eq!(Some(0), v.remove(0));
+        println!("{:?}", v);
+        assert_eq!(Some(1), v.remove(0));
+        println!("{:?}", v);
+        assert_eq!(Some(2), v.remove(0));
+        println!("{:?}", v);
+        assert_eq!(Some(3), v.remove(0));
+        println!("{:?}", v);
+        // Vec <=> Linked 转换
+        let mut v = Linked::from(vec![0, 1, 2, 3, 4, 5]);
+        println!("{:?}", v);
+        let mut v: Vec<_> = v.into();
+        println!("{:?}", v);
+        // 删除倒数第 N 节点，这里通过长度查询（干查用双指针，快指针先走 n 快慢一起走 快到头 慢为 n - 1）
+        let mut v = Linked::from(vec![0, 1, 2, 3, 4, 5]);
+        let len = v.len();
+        assert_eq!(Some(4), v.remove(len - 2));
+        // 获取中间节点，这里通过长度查询(干查用双指针，起点 head 快走 2 步 慢走 1 步，快到头 慢为中间节点)
+        let mut v = Linked::from(vec![1, 2, 3, 4, 5]);
+        assert_eq!(Some(3), v.index(v.len() / 2));
+        // 合并两个有序列表
+        let v1 = Linked::from(vec![0, 1, 1, 3, 3, 5, 7, 9]);
+        let v2 = Linked::from(vec![1, 2, 2, 4, 4, 6, 8, 10]);
+        let l = Linked::merge_order(v1, v2).unwrap();
+        println!("{:?}", l);
+        let v3: Vec<_> = l.into();
+        assert_eq!(vec![0, 1, 1, 1, 2, 2, 3, 3, 4, 4, 5, 6, 7, 8, 9, 10], v3);
+        // 索引地址插入
+        let mut v = Linked::from(vec![0]);
+        v.insert(0, 1);
+        println!("{:?}", v);
+        v.insert(1, 3);
+        println!("{:?}", v);
+        // 反转
+        let mut v = Linked::from(vec![1, 2, 3, 4, 5]);
+        v.reverse();
+        println!("{:?}", v);
+        let v1: Vec<_> = v.into();
+        let v2 = vec![5, 4, 3, 2, 1];
+        assert_eq!(v1, v2);
     }
 }
--- src/linked.rs
+++ src/linked.rs
@@ -2,55 +2,136 @@ use std::fmt::Debug;
 
 // 定义节点
 #[derive(Debug, Clone)]
-struct Node<T> where T: Clone + Debug {
+struct Node<T> where T: Clone + Debug + PartialOrd {
     element: T,
     next: Option<Box<Node<T>>>,
 }
 
-impl<T> Node<T> where T: Clone + Debug {
+impl<T> Node<T> where T: Clone + Debug + PartialOrd {
     fn new(elt: T) -> Self {
         Node {
             element: elt,
             next: None,
         }
     }
+    // 返回最后一个节点
     fn last(&mut self) -> &mut Self {
         if let Some(ref mut node) = self.next {
             return node.last();
         }
         self
     }
-
-    fn index(&mut self, n: usize, i: usize) -> Option<&mut Self> {
+    // 根据索引下标查找节点
+    fn search(&mut self, n: usize, i: usize) -> Option<&mut Self> {
+        // 坐标相同时返回
         if n == i {
             return Some(self);
-        }
-        if let Some(ref mut node) = self.next {
-            node.index(n + 1, i);
+        } else {
+            // 递归调用 所有权机制循环难以处理
+            if let Some(ref mut node) = self.next {
+                return node.search(n + 1, i);
+            }
         }
         None
     }
 }
 
+// 合并有序 Node 递归实现
+impl<T> Node<T> where T: Clone + Debug + PartialOrd {
+    fn merge(n: Option<Box<Node<T>>>, m: Option<Box<Node<T>>>) -> Option<Box<Node<T>>> {
+        match (n, m) {
+            (Some(x), None) => {
+                Some(x)
+            }
+            (None, Some(y)) => {
+                Some(y)
+            }
+            (Some(x), Some(y)) => {
+                if x.element > y.element {
+                    Some(Box::from(
+                        Node {
+                            element: y.element,
+                            next: Node::merge(Some(x), y.next),
+                        }
+                    ))
+                } else {
+                    Some(Box::from(
+                        Node {
+                            element: x.element,
+                            next: Node::merge(x.next, Some(y)),
+                        }
+                    ))
+                }
+            }
+            (_, _) => {
+                None
+            }
+        }
+    }
+}
+
 // 单链表 head 头节点 len 长度
 #[derive(Debug, Clone)]
-pub struct Linked<T> where T: Clone + Debug {
-    head: Option<Node<T>>,
+pub struct Linked<T> where T: Clone + Debug + PartialOrd {
+    head: Option<Box<Node<T>>>,
     len: usize,
 }
 
-impl<T> Linked<T> where T: Clone + Debug {
+// Vec => Linked 转换
+impl<T> From<Vec<T>> for Linked<T> where T: Clone + Debug + PartialOrd {
+    fn from(v: Vec<T>) -> Self {
+        let mut l = Linked::new();
+        for i in v {
+            l.push(i)
+        }
+        l
+    }
+}
+
+// Linked => Vec 转换
+impl<T> Into<Vec<T>> for Linked<T> where T: Clone + Debug + PartialOrd {
+    fn into(mut self) -> Vec<T> {
+        let mut v = Vec::new();
+        for i in 0..self.len {
+            if let Some(n) = self.index(i) {
+                v.push(n);
+            }
+        }
+        v
+    }
+}
+
+impl<T> Linked<T> where T: Clone + Debug + PartialOrd {
     pub fn new() -> Self {
         Linked {
             head: None,
             len: 0,
         }
     }
-
+    // 反转
+    pub fn reverse(&mut self) {
+        // head 不为 None 进行反转处理
+        if let Some(_) = self.head {
+            // 从 head 的下一个节点开始，因已经判断 Some 则 unwrap 无风险
+            let mut node = self.head.as_mut().unwrap().next.take();
+            // 节点 不为 None 一直循环
+            while node.is_some() {
+                // take 所有权 给 temp
+                let mut temp = node.take().unwrap();
+                // 下一节点 指向 node
+                node = temp.next;
+                // head
+                temp.next = self.head.take();
+                self.head = Some(temp);
+            }
+        }
+    }
+    // 末尾压入节点
     pub fn push(&mut self, n: T) {
+        // 根据 head 判断插入方式
         match self.head {
             None => {
-                self.head = Some(Node::new(n));
+                self.head = Some(Box::from(Node::new(n)));
             }
             Some(ref mut head) => {
                 let last = head.last();
@@ -59,11 +140,64 @@ impl<T> Linked<T> where T: Clone + Debug {
         }
         self.len += 1;
     }
-
+    // 根据索引下标删除节点
+    pub fn remove(&mut self, i: usize) -> Option<T> {
+        if let Some(ref mut head) = self.head {
+            // 处理索引为 0 的情况
+            if i == 0 {
+                let node = head.element.clone();
+                self.head = head.next.take();
+                self.len -= 1;
+                return Some(node);
+            } else {
+                // 处理其他情况
+                if let Some(prev) = head.search(0, i - 1) {
+                    // 临时变量 next 存储节点
+                    if let Some(next) = prev.next.take() {
+                        // prev = next => next
+                        prev.next = next.next;
+                        self.len -= 1;
+                        return Some(next.element.clone());
+                    }
+                }
+            }
+        }
+        None
+    }
+    // 索引插入节点
+    pub fn insert(&mut self, i: usize, n: T) -> Option<()> {
+        let mut new = Node::new(n);
+        if let Some(ref mut head) = self.head {
+            // 索引为 0 相当于链表头插入
+            if i == 0 {
+                // 通过 take 取出 head 内的值
+                new.next = self.head.take();
+                self.head = Some(Box::from(new));
+            } else {
+                if let Some(prev) = head.search(0, i - 1) {
+                    if let Some(next) = prev.next.take() {
+                        new.next = Option::from(next);
+                        prev.next = Some(Box::from(new));
+                    }
+                }
+            }
+            self.len += 1;
+        }
+        None
+    }
+    // 末尾删除节点
     pub fn pop(&mut self) -> Option<T> {
         if let Some(ref mut head) = self.head {
+            // 处理只有一个节点的情况
+            if self.len == 1 {
+                let last = head.element.clone();
+                self.head = head.next.take();
+                self.len -= 1;
+                return Some(last);
+            }
+            // 处理其他情况
             let last = head.last().element.clone();
-            if let Some(prev) = head.index(0, self.len - 2) {
+            if let Some(prev) = head.search(0, self.len - 2) {
                 prev.next = None;
                 self.len -= 1;
                 return Some(last);
@@ -71,14 +205,33 @@ impl<T> Linked<T> where T: Clone + Debug {
         }
         None
     }
-
+    // 根据索引下标查找节点
     pub fn index(&mut self, i: usize) -> Option<T> {
-        if let Some(ref mut head) = self.head {
-            if i < self.len {
-                if let Some(node) = head.index(0, i) {
-                    return Some(node.element.clone());
+        if self.len > i {
+            if let Some(ref mut head) = self.head {
+                if let Some(elt) = head.search(0, i) {
+                    return Some(elt.element.clone());
                 }
             }
+        }
+        None
+    }
+    // 获取长度
+    pub fn len(&self) -> usize {
+        self.len
+    }
+}
+
+impl<T> Linked<T> where T: Clone + Debug + PartialOrd {
+    // 合并有序列表
+    pub fn merge_order(n: Linked<T>, m: Linked<T>) -> Option<Linked<T>> {
+        if let Some(node) = Node::merge(n.head, m.head) {
+            // 调用 Node::merge
+            return Some(Linked {
+                head: Some(node),
+                // 合并长度
+                len: n.len + m.len,
+            });
         }
         None
     }
